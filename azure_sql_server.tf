ea2e0208e95442503103c817404914d35d3c5221
Jorge Luiz da Silva <jlsilva01@yahoo.com.br>
2022-09-21 22:05:25 -0300

    First Commit

---
+++ azure_sql_server.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_mssql_server" "sql" {
+  name                = "satc-sqlserver-jorge"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  version             = "12.0"
+
+  administrator_login          = var.usuario_admin
+  administrator_login_password = var.password
+}
+
+resource "azurerm_mssql_database" "sql" {
+  name      = "dados"
+  server_id = azurerm_mssql_server.sql.id
+  collation = "SQL_Latin1_General_CP1_CI_AS"
+
+  auto_pause_delay_in_minutes = -1
+  max_size_gb                 = 32
+  min_capacity                = 10
+  read_replica_count          = 0
+  read_scale                  = false
+  sku_name                    = "GP_S_Gen5_10"
+  zone_redundant              = false
+
+}
+
+resource "azurerm_mssql_firewall_rule" "sql" {
+  name             = "RegraInternetJorgeCasa"
+  server_id        = azurerm_mssql_server.sql.id
+  start_ip_address = "191.187.88.83"
+  end_ip_address   = "191.187.88.83"
+}
---
+++ variables.tf
@@ -0,0 +1,18 @@
+# Lista de variaveis utilizadas nos arquivos de terraform
+variable "resource_group_name" {
+  default = "learn-ff92dab0-3eaa-4a5c-b84c-0b52603e8d6d"
+}
+
+variable "location" {
+  default = "Brazil South"
+}
+
+variable "usuario_admin" {
+  default = "admin_user"
+}
+
+variable "password" {
+  default = "senha@2022"
+}
+
+
